da0f7da0e207152758495acc457fac97e9772484
StjepanDev <stjepansuhic@yahoo.com>
2023-01-13 10:49:29 +0100

    scraping some  links

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 console.log(123);
-
+const fs = require('fs')
 const pupeteer = require('puppeteer');
 
 async function run() {
     const browser = await pupeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.24sata.hr/');
+    await page.goto('https://www.vecernji.hr');
 
 
     // await page.screenshot({ path:'screenshots/example.png', fullPage: true});
@@ -13,8 +13,19 @@ async function run() {
     // const html = await page.content();
 
 
-    const pageTitle = await page.evaluate(() => document.title);
-    console.log(pageTitle);
+    // const pageTitle = await page.evaluate(() => document.title);
+    // console.log(pageTitle);
+
+    // const text = await page.evaluate(()=> document.body.innerText)
+    const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), e => e.href))
+
+    console.log(links)
+
+    fs.writeFile('links.json', JSON.stringify(links),(err) => {
+        if (err) throw err;
+        console.log('Saved')
+    })
+
 
 
  
